96e805bbd8c9ca68e4bd47f3773d7d12eaa3887a
AmenyoSedem <sedemamenyo28@gmail.com>
2024-08-26 22:05:42 +0000

    Created an animation using framer motion

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import aboutImg from '../assets/about.jpg';
 import { aboutContent } from '../constants';
+import { motion } from 'framer-motion';
 
 function About() {
   return (
@@ -8,16 +9,25 @@ function About() {
             <span className="text-neutral-500"> Me</span>
         </h1>
         <div className="flex flex-wrap">
-            <div className="w-full lg:w-1/2 lg:p-8">
+            <motion.div 
+                initial={{ x: -100, opacity: 0 }}
+                whileInView={{ x:0, opacity: 1 }}
+                transition={{ duration:0.5, delay: 0 }}
+                className="w-full lg:w-1/2 lg:p-8"
+            >
                 <div className="flex items-center justify-center">
                     <img className='rounded-2xl' src={aboutImg} alt="About Image" />
                 </div>           
-            </div>
-            <div className="w-full lg:w-1/2">
+            </motion.div>
+            <motion.div 
+                initial={{ x: 100, opacity: 0 }}
+                whileInView={{ x:0, opacity: 1 }}
+                transition={{ duration:0.5, delay: 0 }}
+                className="w-full lg:w-1/2">
                 <div className="flex justify-center lg:justify-start">
                     <p className='my-2 max-w-xl py-6'>{aboutContent}</p>
                 </div>
-            </div>
+            </motion.div>
         </div>
     </div>
   )
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -5,33 +5,88 @@ import { DiRedis } from "react-icons/di";
 import { FaNodeJs } from "react-icons/fa";
 import { BiLogoPostgresql } from "react-icons/bi";
 import { DiDjango } from "react-icons/di";
+import { animate, motion } from "framer-motion";
+
+const iconVariants = (duration) => ({
+    initial: {y: -10},
+    animate: {
+        y: [10, -10],
+        transition: {
+            duration: duration,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "reverse"
+        }
+    }
+})
 
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
         <h1 className="my-20 text-center text-4xl ">Technologies</h1>
         <div className="flex flex-wrap items-center justify-center gap-4">
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            <motion.div
+                variants={iconVariants(2.5)}
+                initial="initial"
+                animate="animate" 
+                className="rounded-2xl border-4 border-neutral-800 p-4"
+            >
                 <RiReactjsLine className="text-4xl text-cyan-400"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+
+            <motion.div 
+                variants={iconVariants(3)}
+                initial="initial"
+                animate="animate" 
+                className="rounded-2xl border-4 border-neutral-800 p-4"
+            >
                 <TbBrandNextjs className="text-4xl"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+
+            <motion.div 
+                variants={iconVariants(3.5)}
+                initial="initial"
+                animate="animate" 
+                className="rounded-2xl border-4 border-neutral-800 p-4"
+            >
                 <DiMongodb className="text-4xl text-green-500"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+
+            <motion.div 
+                variants={iconVariants(4)}
+                initial="initial"
+                animate="animate" 
+                className="rounded-2xl border-4 border-neutral-800 p-4"
+            >
                 <DiRedis className="text-4xl text-red-700"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+
+            <motion.div 
+                variants={iconVariants(4.5)}
+                initial="initial"
+                animate="animate" 
+                className="rounded-2xl border-4 border-neutral-800 p-4"
+            >
                 <FaNodeJs className="text-4xl text-green-500"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+
+            <motion.div 
+                variants={iconVariants(5)}
+                initial="initial"
+                animate="animate" 
+                className="rounded-2xl border-4 border-neutral-800 p-4"
+            >
                 <BiLogoPostgresql className="text-4xl text-sky-600"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+
+            <motion.div 
+                variants={iconVariants(5.5)}
+                initial="initial"
+                animate="animate" 
+                className="rounded-2xl border-4 border-neutral-800 p-4"
+            >
                 <DiDjango className="text-4xl text-green-900"/>
-            </div>
+            </motion.div>
         </div>
     </div>
   )
