94b4b1ee311575070ac66e78c0071c1cc0227790
AmenyoSedem <sedemamenyo28@gmail.com>
2024-08-27 18:42:23 +0000

    Updated the Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { motion } from 'framer-motion'
+import React from 'react';
+import { motion } from 'framer-motion';
 
 const Footer = () => {
   return (
-    <div className='border-t border-neutral-700 text-center py-20'>
-        <p className='text-xl'>&copy; {new Date().getFullYear()} Amenyo Sedem</p>
+    <div className='border-t border-neutral-700 text-center py-10 sm:py-20'>
+        <p className='text-xl text-neutral-400'>&copy; {new Date().getFullYear()} Amenyo Sedem</p>
     </div>
   )
 }
 
-export default Footer
+export default Footer;
