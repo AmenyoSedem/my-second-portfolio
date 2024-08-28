e29e5718df4f56c3d2615e9e1e2c9bed2bf2ed38
AmenyoSedem <sedemamenyo28@gmail.com>
2024-08-28 13:59:17 +0000

    Updated Footer.jsx again

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 const Footer = () => {
   return (
     <div className='border-t border-neutral-700 text-center py-10 sm:py-20'>
-        <p className='text-l sm:text-xl text-neutral-400'>&copy; {new Date().getFullYear()} Amenyo Sedem. All rights reversed.</p>
+        <p className='text-l sm:text-xl text-neutral-400'>&copy; {new Date().getFullYear()} Amenyo Sedem. <br /> All rights reversed.</p>
     </div>
   )
 }
